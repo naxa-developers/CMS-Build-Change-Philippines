858877df7401c6752d7a1de71b6bfdb32c33d3c3
awemulya <awemulya@gmail.com>
2018-05-11 11:26:41 +0545

    steps configure

--- survey/core/urls.py
+++ survey/core/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('site-list/', views.SiteListView.as_view(), name="site_list"),
     path('site-delete/<int:pk>/', views.SiteDeleteView.as_view(), name="site_delete"),
     path('site-detail/<int:pk>/', views.SiteDetailView.as_view(), name="site_steps"),
-    path('site-steps/<int:pk>/', views.SiteStepsView.as_view(), name="site_detail")
+    path('steps/<int:is_project>/<int:pk>/', views.SiteStepsView.as_view(), name="site_detail")
 ]
