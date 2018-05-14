56360051052e83628cdfb419df703f4911143ee9
JoPuskar <puskarjoshi22@gmail.com>
2018-05-14 18:06:52 +0545

    material category CRUD completed

--- survey/userrole/admin.py
+++ survey/userrole/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
 from userrole.models import UserRole
-from core.models import Project, Site, Step
+from core.models import Project, Site, Step, Category, Material
+
+# Register your models here.
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    model = Material
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = Material.objects.filter(project=request.object.project.pk)
+        return super(MyModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(UserRole)
 admin.site.register(Project)
 admin.site.register(Site)
 admin.site.register(Step)
+admin.site.register(Category)
+admin.site.register(Material, MyModelAdmin)
