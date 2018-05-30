03a707eb9bb22e86d369a591f394311b7ec51a74
Pramod Tiwari <you@tpromode@gmai.com>
2018-05-30 11:53:58 +0545

    UserProfileUpdateView edit

--- survey/core/views.py
+++ survey/core/views.py
@@ -757,3 +757,9 @@ class UserProfileUpdateView(UpdateView):
     model = User
     fields = ['first_name', 'last_name']
     success_url = reverse_lazy("home")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['project_id'] = self.request.project.pk
+        return context
