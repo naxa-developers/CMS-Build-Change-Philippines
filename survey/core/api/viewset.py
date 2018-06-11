c22d8ff18bcf702bdad52f77ffcc950cb51ee233
raeesaroj <raesaroj16@gmail.com>
2018-06-11 17:07:57 +0545

    user signup api

--- survey/core/api/viewset.py
+++ survey/core/api/viewset.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from rest_framework import viewsets, serializers
 from rest_framework.permissions import AllowAny
@@ -17,7 +17,26 @@ from .serializers import ProjectSerializer, StepsSerializer, MaterialSerializer,
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('url', 'username', 'password', 'email')
+
+        write_only_fields = ['password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 
 @permission_classes((AllowAny, ))
