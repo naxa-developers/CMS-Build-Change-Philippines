7dff95512a140a200b5dccf9fbbfad486c715afb
raeesaroj <raesaroj16@gmail.com>
2018-06-28 16:02:02 +0545

    last_submission update

--- survey/core/api/serializers.py
+++ survey/core/api/serializers.py
@@ -96,7 +96,7 @@ class ChecklistSerializer(serializers.ModelSerializer):
         return {}
 
     def get_last_submission(self, obj):
-        latest_submission = obj.checklist_report.filter()[:1].get()
+        latest_submission = obj.checklist_report.all().first()
         if latest_submission:
             return ReportSerializer(latest_submission).data
         return {}
