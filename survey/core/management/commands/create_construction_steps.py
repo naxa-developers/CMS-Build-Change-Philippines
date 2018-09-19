25d5bba704fea4ec2bcaf44acdb808460609f6ae
raeesaroj <raesaroj16@gmail.com>
2018-09-18 20:35:06 -0400

    management commands

--- survey/core/management/commands/create_construction_steps.py
+++ survey/core/management/commands/create_construction_steps.py
@@ -1,22 +1,27 @@
 from django.core.management.base import BaseCommand
 
-from core.models import ConstructionStepsTitle
+from core.models import ConstructionSteps
 
 
 class Command(BaseCommand):
     help = 'Create default construction_steps'
 
+    def add_arguments(self, parser):
+        parser.add_argument('project_id', type=int)
+
     def handle(self, *args, **options):
+        project_id = options['project_id']
+        print(project_id)
 
-        CONSTRUCTION_STEPS_LIST = \
-            ["Construction Of Ring Beams",
-             "Construction Of Lintel Beams On The Openings",
-             "Electrical Works",
-             "Installation Of Ceiling",
-             "Paint Works",
-
-             ]
-        for construction_step in CONSTRUCTION_STEPS_LIST:
-            new_construction_step, created = ConstructionStepsTitle.objects.get_or_create(title=construction_step)
-            if created:
-                self.stdout.write('Successfully created construction_step .. "%s"' % construction_step)
+        # CONSTRUCTION_STEPS_LIST = \
+        #     ["Construction Of Ring Beams",
+        #      "Construction Of Lintel Beams On The Openings",
+        #      "Electrical Works",
+        #      "Installation Of Ceiling",
+        #      "Paint Works",
+        #
+        #      ]
+        # for construction_step in CONSTRUCTION_STEPS_LIST:
+        #     new_construction_step, created = ConstructionSteps.objects.get_or_create(title=construction_step, project_id=project_id)
+        #     if created:
+        #         self.stdout.write('Successfully created construction_step .. "%s"' % construction_step)
