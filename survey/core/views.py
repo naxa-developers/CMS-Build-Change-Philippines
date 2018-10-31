b59e62faed3f889e0ad0bdae9c8b411dae13fede
raeesaroj <raesaroj16@gmail.com>
2018-10-31 02:50:21 -0400

    houses-and-general-construction model images and file in zip

--- survey/core/views.py
+++ survey/core/views.py
@@ -22,7 +22,7 @@ from survey.settings import BASE_DIR
 from userrole.forms import UserProfileForm
 from userrole.models import UserRole
 from .models import Project, Site, Category, Material, Step, Report, SiteMaterials, SiteDocument, Checklist, ConstructionSteps, \
-    ConstructionSubSteps, CONSTRUCTION_STEPS_LIST, CONSTRUCTION_SUB_STEPS_LIST, SiteSteps, SubStepCheckList, SubstepReport
+    ConstructionSubSteps, CONSTRUCTION_STEPS_LIST, CONSTRUCTION_SUB_STEPS_LIST, SiteSteps, SubStepCheckList, SubstepReport, HousesAndGeneralConstructionMaterials, BuildAHouseMakesHouseStrong, BuildAHouseKeyPartsOfHouse
 from .forms import ProjectForm, CategoryForm, MaterialForm, SiteForm, SiteMaterialsForm, SiteDocumentForm, \
     UserCreateForm, SiteConstructionStepsForm, ConstructionSubStepsForm, PrimaryPhotoFormset, SubStepCheckListForm, BadPhotoFormset, GoodPhotoFormset
 from .rolemixins import ProjectRoleMixin, SiteRoleMixin, CategoryRoleMixin, ProjectGuidelineRoleMixin, \
@@ -71,6 +71,9 @@ def project_material_photos(request, project_id):
     material_photos = ConstructionSubSteps.objects.filter(project_id=project_id)
     project = get_object_or_404(Project, id=project_id)
     step_image = ConstructionSteps.objects.filter(project_id=project_id)
+    more_about_materials = HousesAndGeneralConstructionMaterials.objects.all()
+    my_house_strong = BuildAHouseMakesHouseStrong.objects.all()
+    key_parts_of_house = BuildAHouseKeyPartsOfHouse.objects.all()
 
     for filename in material_photos:
         if filename.good_photos:
@@ -86,6 +89,25 @@ def project_material_photos(request, project_id):
     for img in step_image:
         if img.image:
             zip_file.write(os.path.join(BASE_DIR) + img.image.url, arcname=img.image.url)
+    
+    for img in more_about_materials:
+        if img.good_photo:
+            zip_file.write(os.path.join(BASE_DIR) + img.good_photo.url, arcname=img.good_photo.url)
+        
+        if img.bad_photo:
+            zip_file.write(os.path.join(BASE_DIR) + img.bad_photo.url, arcname=img.bad_photo.url)
+
+    for img in key_parts_of_house:
+        if img.good_photo:
+            zip_file.write(os.path.join(BASE_DIR) + img.good_photo.url, arcname=img.good_photo.url)
+        
+        if img.bad_photo:
+            zip_file.write(os.path.join(BASE_DIR) + img.bad_photo.url, arcname=img.bad_photo.url)
+
+    for data in my_house_strong:
+        if data.pdf:
+            zip_file.write(os.path.join(BASE_DIR) + data.pdf.url, arcname=data.pdf.url)
+
 
     response['Content-Disposition'] = 'attachment; filename={}MaterialPhotos.zip'.format(project.name)
     return response
