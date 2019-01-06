1a87533840cc39483149b6f04616413382a6d587
Pramod Tiwari <tpromode@gmail.com>
2019-01-06 10:35:09 +0545

    update notification

--- survey/core/context_processors.py
+++ survey/core/context_processors.py
@@ -3,7 +3,9 @@ from core.models import SubstepReport
 def reports_list(request):
     try:
         reports = SubstepReport.objects.all().order_by('-pk')[:5]
+        count = SubstepReport.objects.all().count()
     except:
         reports = None
+        count = None
 
-    return {'reports': reports}
+    return {'reports': reports, 'count':count}
