8c582f28ece7be6b07949bc0adf40439f348bf5d
raeesaroj <raesaroj16@gmail.com>
2019-01-09 14:32:07 +0545

    context processor update

--- survey/core/context_processors.py
+++ survey/core/context_processors.py
@@ -3,18 +3,17 @@ from core.models import Notification
 def reports_list(request):
     rep_dict = {}
     rep_list = []
+    count = 0
     try:
-        notification = Notification.objects.filter(
-            user=request.user, read=False
-            ).order_by('-pk')[:5]
+        notification = Notification.objects.filter(read=False).order_by('-pk')[:5]
         if notification:
             for item in notification:
                 rep_dict['notif'] = item
                 rep_dict['pk'] = item.report_id
                 rep_list.append(dict(rep_dict))
             count = notification.count()
+
     except:
         reports = None
-        count = None
 
     return {'notifications': rep_list, 'count':count}
